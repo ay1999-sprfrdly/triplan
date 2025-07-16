32816543509b0b02e2b125102c447b8fdf8715c9
sundub-ay <sundubtbti@gmail.com>
2025-07-16 23:50:34 +0900

    modify デプロイエラー解消

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      optimizeDeps: {
+        include: ['firebase/app', 'firebase/firestore'],
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
